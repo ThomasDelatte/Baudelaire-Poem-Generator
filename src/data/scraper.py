1506cc1e0d8823a86bc15bec94b3193568f99014
ThomasDelatte <thomasdelatte@hotmail.com>
2020-04-07 12:14:06 +0200

    Add data folder

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -2,15 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def scrape_poems():
+def scrape_poems(url):
 
-    get_urls = requests.get("https://www.poesie-francaise.fr/poemes-charles-baudelaire/")
+    get_urls = requests.get(url)
 
     soup_url = BeautifulSoup(get_urls.text, "html.parser")
 
     links = []
 
-    for link in soup_url.findAll('a', attrs={'href': re.compile("^https://www.poesie-francaise.fr/charles-baudelaire/")}):
+    for link in soup_url.findAll('a', attrs={'href': re.compile("^" + url)}):
         links.append(link.get('href'))
 
     extracted_poems = []
@@ -27,9 +27,5 @@ def scrape_poems():
             poem = str(poem).replace(i, j)
         extracted_poems.append(title)
         extracted_poems.append(poem)
-    
-    with open("/home/tdelatte/Projects/Deep_Learning/NLP/Baudelaire_Poem_Generator/data/raw/baudelaire.txt", "w") as f:
-        for line in extracted_poems:
-            f.write("%s\n" % line)
-    
-    print("Poems scraped from the web!")
+        
+    return extracted_poems
